7e575a12605c2c023a43c7fcf215ed55676a9ca8
Eniko Nagy <eniko@iwa.fi>
2021-03-21 21:04:56 +0700

    fix identicon

--- src/components/common/Identicon.tsx
+++ src/components/common/Identicon.tsx
@@ -1,8 +1,7 @@
-import React  from 'react'
+import React from 'react'
 import { useActiveWeb3React } from '../../hooks'
-import { Jazzicon } from '@ukstv/jazzicon-react';
+import { Jazzicon } from '@ukstv/jazzicon-react'
 import styled from 'styled-components'
-
 
 const StyledIdenticonContainer = styled.div`
   height: 1rem;
@@ -11,13 +10,19 @@ const StyledIdenticonContainer = styled.div`
   background-color: ${({ theme }) => theme.bg4};
 `
 
-export function Identicon() {
-
+export function IdenticonUnstyled({ className }: { className?: string }) {
   const { account } = useActiveWeb3React()
 
-  const seed = account ? parseInt(account.slice(2, 10), 16).toString(): ""
+  const seed = account ? parseInt(account.slice(2, 10), 16).toString() : ''
   // https://github.com/DefinitelyTyped/DefinitelyTyped/issues/30451
-  return <StyledIdenticonContainer >
-    <Jazzicon address={seed}/>
-  </StyledIdenticonContainer>
+  return (
+    <StyledIdenticonContainer>
+      <Jazzicon address={seed} className={className} />
+    </StyledIdenticonContainer>
+  )
 }
+
+// Fix height of jazzicon through className
+export const Identicon = styled(IdenticonUnstyled)`
+  height: 1rem !important;
+`
